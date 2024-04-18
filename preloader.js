55ca1fe5ac9573e445491745e93f01d0b2d181e8
Masane777 <47014083+Masane777@users.noreply.github.com>
2024-04-18 13:50:55 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -7,8 +7,8 @@ overlay.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
-    width: 200px;
-    height: 200px;
+    width: 100px;
+    height: 100px;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
